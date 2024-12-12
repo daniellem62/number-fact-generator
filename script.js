3f4d60fc19407523c2769c9c0d227fb97096fbf3
Danielle Mcloughlin <184102558+daniellem62@users.noreply.github.com>
2024-12-12 14:21:29 +0000

    Added image and css styling

--- script.js
+++ script.js
@@ -16,14 +16,13 @@ async function getFact() {
     console.log(`error: ${error.message}`);
   }
 }
-getFact();
 
 const newTriviaButton = document.getElementById("triviaBtn");
 //fetch the api in a variable
 //create another variable for the JSON
 //Return result
 //When the user presses a button
-newTriviaButton.addEventListener("click", getFact);
+newTriviaButton.addEventListener("click", getAndDisplayFact);
 //Then diplay fact as text into a field
 function displayFact(fact) {
   const factElement = document.getElementById("fact");
