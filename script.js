bd4157317d1346f396c7fa1ec8c75dabfbf7e211
davidsherry14 <davidsherry1407@gmail.com>
2024-12-12 13:47:30 +0000

    small changes

--- script.js
+++ script.js
@@ -3,26 +3,33 @@
 
 //Test pulling from the API
 async function getFact() {
-    try {
+  try {
     const response = await fetch(`http://numbersapi.com/random?json`, {
-        headers: {
-            Accept: "application/JSON",
-        },
+      headers: {
+        Accept: "application/JSON",
+      },
     });
     const data = await response.json();
     console.log(data.text);
-    return data;
-} catch (error) {
+    return data.text;
+  } catch (error) {
     console.log(`error: ${error.message}`);
-}
+  }
 }
 getFact();
 
+const newTriviaButton = document.getElementById("triviaBtn");
 //fetch the api in a variable
-//create another variable for the JSON 
+//create another variable for the JSON
 //Return result
-
 //When the user presses a button
+newTriviaButton.addEventListener("click", getFact);
 //Then diplay fact as text into a field
-
-
+function displayFact(fact) {
+  const factElement = document.getElementById("fact");
+  factElement.textContent = fact;
+}
+async function getAndDisplayFact() {
+  const fact = await getFact();
+  displayFact(fact);
+}
